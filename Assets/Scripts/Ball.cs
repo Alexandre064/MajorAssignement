60a6f57022efd8c53e2e18c8e27449bf84c37bf7
Milan <milan@mucka.sk>
2019-12-05 17:26:52 +0100

    Bricks Braker game init

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5f;
+    public int score = 0;
+    public TMP_Text ScoreUI;
+
+    void Start()
+    {
+        float x = (Random.Range(0, 2) == 0) ? -1 : 1; //init movement vector path
+        float y = (Random.Range(0, 2) == 0) ? -1 : 1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(x, y, 0);
+        GetComponent<Rigidbody>().velocity *= speed; //init speed
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "SideWallB")
+        {
+            score = 0;  //lose,make new scene
+        }
+        else if (collision.gameObject.name != "Bumper" 
+            && collision.gameObject.name != "SideWallR" 
+            && collision.gameObject.name != "SideWallL" 
+            && collision.gameObject.name != "SideWallT" )
+        {
+            collision.gameObject.SetActive(false);
+            ScoreUI.text = (++score).ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public float speed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis($"Horizontal") * speed * Time.deltaTime;
+        transform.Translate(x, 0f, 0f);
+    }
+}
